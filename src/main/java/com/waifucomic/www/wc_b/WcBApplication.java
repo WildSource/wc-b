f7b96ea9d8591208006ac0d4e45e551468227995
WildSource <ilovetrap123@hotmail.com>
2024-10-16 20:10:22 -0400

    initial project

---
+++ src/main/java/com/waifucomic/www/wc_b/WcBApplication.java
@@ -0,0 +1,13 @@
+package com.waifucomic.www.wc_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waifucomic/www/wc_b/WcBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waifucomic.www.wc_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
